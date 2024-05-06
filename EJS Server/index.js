d15a029d690697ba0ce203da6c459474f6873c33
carolina-bolnykh <91427069+carolina-bolnykh@users.noreply.github.com>
2024-05-06 12:28:26 -0700

    Update index.js

--- EJS Server/index.js
+++ EJS Server/index.js
@@ -1,36 +1,46 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
-
 import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 const port = 3000;
-var pass = "";
-app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, () => {});
+var userIsAuthorised = false;
 
-function authenticate(req, res, next) {
-    console.log(req);
-    pass = req.body["password"];
-    if(pass == "ILoveProgramming"){
-        res.sendFile(__dirname + "/public/secret.html");
-    } else {
-        res.sendFile(__dirname + "/public/index.html");
-    }
-    next();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function passwordCheck(req, res, next) {
+  console.log("checing!");
+  const password = req.body["password"];
+  if (password === "ILoveProgramming") {
+    userIsAuthorised = true;
+  } else {
+    userIsAuthorised = false;
+  }
+  next();
 }
-
+app.use(passwordCheck);
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
-})
+  res.sendFile(__dirname + "/public/index.html");
+  console.log(req.body);
+});
 
 app.post("/check", (req, res) => {
-    // console.log(req.body);
-    app.use(authenticate);
-})
+  console.log(req.body);
+  if (userIsAuthorised) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.render("index.ejs", 
+    // send the request to render index.ejs
+    {password: req.body["password"]}, 
+    );
+    //Alternatively res.redirect("/");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
